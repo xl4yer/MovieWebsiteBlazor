d5983cba908226d5aa379b4290db3902ad62ee7c
jmescolano <escolanojademark17@outlook.com>
2024-07-19 15:01:23 +0800

    ..

--- Controllers/MovController.cs
+++ Controllers/MovController.cs
@@ -16,15 +16,64 @@ namespace Mov.Controllers
         }
 
         [HttpGet]
-        public async Task<List<mov>> MovieList()
+        public async Task<List<mov>> Movies()
         {
-            var ret = await xservices.MovieList();
+            var ret = await xservices.Movies();
             return ret;
         }
         [HttpGet]
-        public async Task<List<mov>> MovSearch(string search)
+        public async Task<List<mov>> Search(string search)
         {
-            var ret = await xservices.MovSearch(search);
+            var ret = await xservices.Search(search);
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Action()
+        {
+            var ret = await xservices.Action();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Adventure()
+        {
+            var ret = await xservices.Adventure();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Animation()
+        {
+            var ret = await xservices.Animation();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Comedy()
+        {
+            var ret = await xservices.Comedy();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Crime()
+        {
+            var ret = await xservices.Crime();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Documentary()
+        {
+            var ret = await xservices.Documentary();
+            return ret;
+        }
+
+        [HttpGet]
+        public async Task<List<mov>> Drama()
+        {
+            var ret = await xservices.Drama();
             return ret;
         }
     }
--- Services/MovServices.cs
+++ Services/MovServices.cs
@@ -16,7 +16,7 @@ namespace Mov.Services
             Configuration = configuration;
         }
 
-        public async Task<List<mov>> MovieList()
+        public async Task<List<mov>> Movies()
         {
             List<mov> mov = new List<mov>();
             using (var con = new MySqlConnection(_constring.GetConnection()))
@@ -24,7 +24,7 @@ namespace Mov.Services
                 try
                 {
                     await con.OpenAsync().ConfigureAwait(false);
-                    var com = new MySqlCommand("MovList", con)
+                    var com = new MySqlCommand("Movies", con)
                     {
                         CommandType = CommandType.StoredProcedure,
                     };
@@ -55,7 +55,7 @@ namespace Mov.Services
             return mov;
         }
 
-        public async Task<List<mov>> MovSearch(string search)
+        public async Task<List<mov>> Search(string search)
         {
             List<mov> mov = new List<mov>();
             using (var con = new MySqlConnection(_constring.GetConnection()))
@@ -63,13 +63,286 @@ namespace Mov.Services
                 try
                 {
                     await con.OpenAsync().ConfigureAwait(false);
-                    var com = new MySqlCommand("MovSearch", con)
+                    var com = new MySqlCommand("Search", con)
                     {
                         CommandType = CommandType.StoredProcedure,
                     };
                     com.Parameters.Clear();
                     com.Parameters.AddWithValue("search", search);
                     com.Parameters.AddWithValue("@searchWildcard", $"{search}%");
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Action()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Action", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Adventure()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Adventure", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Animation()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Animation", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Comedy()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Comedy", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Crime()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Crime", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Documentary()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Documentary", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
+                    var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
+                    while (await rdr.ReadAsync().ConfigureAwait(false))
+                    {
+                        mov.Add(new mov
+                        {
+                            movID = rdr["movID"].ToString(),
+                            photo = (byte[])rdr["photo"],
+                            title = rdr["title"].ToString(),
+                            genre = rdr["genre"].ToString(),
+                            description = rdr["description"].ToString(),
+                            url = rdr["url"].ToString(),
+                        });
+                    }
+                    await rdr.CloseAsync().ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception here
+                }
+                finally
+                {
+                    await con.CloseAsync().ConfigureAwait(false);
+                }
+            }
+            return mov;
+        }
+
+        public async Task<List<mov>> Drama()
+        {
+            List<mov> mov = new List<mov>();
+            using (var con = new MySqlConnection(_constring.GetConnection()))
+            {
+                try
+                {
+                    await con.OpenAsync().ConfigureAwait(false);
+                    var com = new MySqlCommand("Drama", con)
+                    {
+                        CommandType = CommandType.StoredProcedure,
+                    };
                     var rdr = await com.ExecuteReaderAsync().ConfigureAwait(false);
                     while (await rdr.ReadAsync().ConfigureAwait(false))
                     {
